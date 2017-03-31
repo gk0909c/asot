c86dfbfa3fe28dab34a8c6b373b70d308b6ca373
gk0909c <gk0909c@gmail.com>
2017-03-31 13:33:17 +0900

    correct rubocop warnings.

--- lib/asot.rb
+++ lib/asot.rb
@@ -2,6 +2,7 @@ require 'asot/version'
 require 'asot/sfdc_helper'
 require 'asot/rest/connector'
 
+# module for Automate Salesforce Operation Test
 module Asot
   def self.connect(config)
     Rest::Connector.new(config)
--- lib/asot/version.rb
+++ lib/asot/version.rb
@@ -1,3 +1,3 @@
 module Asot
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'.freeze
 end
